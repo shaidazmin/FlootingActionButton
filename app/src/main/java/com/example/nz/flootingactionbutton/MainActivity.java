8ec8ca3ca75586fdc253ff218e8bcd42059dbc75
Shaid Azmin <shaidazmin@gmail.com>
2019-05-03 20:08:55 +0600

    Initial commit

---
+++ app/src/main/java/com/example/nz/flootingactionbutton/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.nz.flootingactionbutton;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You presed \n Floating Action Button",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
